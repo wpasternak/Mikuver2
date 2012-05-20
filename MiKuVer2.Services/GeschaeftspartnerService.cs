a10f15a7d3f2e5c5c35814f8a882dbcabc87561a
Willi Pasternak <willi.pasternak@googlemail.com>
2012-05-20 17:32:43 +0200

    InterfaceMember von ReSharper generieren lassen.

--- MiKuVer2.Services/GeschaeftspartnerService.cs
+++ MiKuVer2.Services/GeschaeftspartnerService.cs
@@ -7,11 +7,75 @@ using System.Text;
 
 namespace MiKuVer2.Services
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "GeschaeftspartnerService" in both code and config file together.
+    using MiKuVer2.Model;
+
     public class GeschaeftspartnerService : IGeschaeftspartnerService
     {
-        public void DoWork()
+        /// <summary>
+        /// Gets or sets GeschaeftspartnerRepository.
+        /// </summary>
+        public IGeschaeftspertnerRepository GeschaeftspartnerRepository { get; set; }
+
+        /// <summary>
+        /// Gibt alle direkten Geschaeftsparner zurueck
+        /// </summary>
+        /// <returns>Liste aller direkten Geschaeftspartner</returns>
+        public List<Geschaeftspartner> GetDirekteGeschaeftspartner()
         {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gibt alle Geschaeftspartner zurueck
+        /// </summary>
+        /// <returns>Liste aller Geschaeftspartner</returns>
+        public List<Geschaeftspartner> GetAlleGeschaeftspartner()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gibt den Geschaeftspartner zurueck
+        /// </summary>
+        /// <param name="id">Die Id des Geschaeftspartners</param>
+        /// <returns>Einen Geschaeftspartner</returns>
+        public Geschaeftspartner GetGeschaeftspartner(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gibt den Geschaeftspartner zurueck
+        /// </summary>
+        /// <param name="name">Der Name des Geschaeftspartners</param>
+        /// <returns> Einen Geschaeftspartner</returns>
+        public Geschaeftspartner GetGeschaeftspartner(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gibt einen Wert zurueck ob der Geschaeftspartner 
+        /// gespeichert wurde
+        /// </summary>
+        /// <param name="neuerGeschaeftspartner">Der zu speichernde Geschaefts-
+        /// partner</param>
+        /// <returns>true oder false</returns>
+        public bool GeschaeftspartnerSpeichern(Geschaeftspartner neuerGeschaeftspartner)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gibt einen Wert zurueck ob der vorhande Geschaeftspartner
+        /// aktualisiert wurde
+        /// </summary>
+        /// <param name="geschaeftspartner">Der vorhande und zu aktualisierende 
+        /// Geschaeftspartner</param>
+        /// <returns>true oder false</returns>
+        public bool GeschaeftspartnerAktualisieren(Geschaeftspartner geschaeftspartner)
+        {
+            throw new NotImplementedException();
         }
     }
 }
