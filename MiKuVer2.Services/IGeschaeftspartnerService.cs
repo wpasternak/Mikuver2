853383bd9ce43d692e326fb81e54505a584c7dbb
Willi Pasternak <willi.pasternak@googlemail.com>
2012-05-20 17:27:28 +0200

    IGeschaeftspartner definiert.

--- MiKuVer2.Services/IGeschaeftspartnerService.cs
+++ MiKuVer2.Services/IGeschaeftspartnerService.cs
@@ -5,11 +5,58 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 
+using MiKuVer2.Model;
+
 namespace MiKuVer2.Services
 {
     [ServiceContract]
     public interface IGeschaeftspartnerService
     {
+        IGeschaeftspertnerRepository GeschaeftspartnerRepository { get; set; }
+
+        /// <summary>
+        /// Gibt alle direkten Geschaeftsparner zurueck
+        /// </summary>
+        /// <returns>Liste aller direkten Geschaeftspartner</returns>
+        List<Geschaeftspartner> GetDirekteGeschaeftspartner();
+
+        /// <summary>
+        /// Gibt alle Geschaeftspartner zurueck
+        /// </summary>
+        /// <returns>Liste aller Geschaeftspartner</returns>
+        List<Geschaeftspartner> GetAlleGeschaeftspartner();
+
+        /// <summary>
+        /// Gibt den Geschaeftspartner zurueck
+        /// </summary>
+        /// <param name="id">Die Id des Geschaeftspartners</param>
+        /// <returns>Einen Geschaeftspartner</returns>
+        Geschaeftspartner GetGeschaeftspartner(int id);
+
+        /// <summary>
+        /// Gibt den Geschaeftspartner zurueck
+        /// </summary>
+        /// <param name="name">Der Name des Geschaeftspartners</param>
+        /// <returns> Einen Geschaeftspartner</returns>
+        Geschaeftspartner GetGeschaeftspartner(string name);
+
+        /// <summary>
+        /// Gibt einen Wert zurueck ob der Geschaeftspartner 
+        /// gespeichert wurde
+        /// </summary>
+        /// <param name="neuerGeschaeftspartner">Der zu speichernde Geschaefts-
+        /// partner</param>
+        /// <returns>true oder false</returns>
+        bool GeschaeftspartnerSpeichern(Geschaeftspartner neuerGeschaeftspartner);
+
+        /// <summary>
+        /// Gibt einen Wert zurueck ob der vorhande Geschaeftspartner
+        /// aktualisiert wurde
+        /// </summary>
+        /// <param name="geschaeftspartner">Der vorhande und zu aktualisierende 
+        /// Geschaeftspartner</param>
+        /// <returns>true oder false</returns>
+        bool GeschaeftspartnerAktualisieren(Geschaeftspartner geschaeftspartner);
 
     }
 }
