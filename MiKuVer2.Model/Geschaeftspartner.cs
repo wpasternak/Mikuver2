54e02d8b276e22c6a680f89cbe68dcc35380bae1
Avedis Sagrjan <awesig@mail.ru>
2012-05-20 16:42:43 +0200

    Geschaeftspartnerklasse hinzugefuegt

---
+++ MiKuVer2.Model/Geschaeftspartner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MiKuVer2.Model
+{
+    /// <summary>
+    /// Die Geschaeftspartnerdaten
+    /// </summary>
+    public class Geschaeftspartner : Person
+    {
+        /// <summary>
+        /// Der Vorgesetzte des jeweiligen Geschaeftspartners
+        /// </summary>
+        public Geschaeftspartner Vorgesetzter { get; set; }
+
+        /// <summary>
+        /// Die untergeordneten Geschaeftspartner des jeweiligen Geschaefts-
+        /// pertners
+        /// </summary>
+        public List<Geschaeftspartner> Partner { get; set; }
+
+        /// <summary>
+        /// Das Eintrittsdatum des Geschaeftspartners in das Unternehmen
+        /// </summary>
+        public DateTime Eintrittsdatum { get; set; }
+    }
+}
