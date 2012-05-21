e7edfc19b717cb977f898a51fbbba5b891986bdb
Willi Pasternak <willi.pasternak@googlemail.com>
2012-05-21 16:08:37 +0200

    Fake Repository fur GPRepository erstellt.

---
+++ MiKuVer2.Repositories.Geschaeftspartner.Fake/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MiKuVer2.Repositories.Geschaeftspartner.Fake
+{
+    public class Class1
+    {
+    }
+}
