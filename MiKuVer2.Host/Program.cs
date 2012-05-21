340124326aa7497b751d8b4cb7a56a0931e5b94e
Willi Pasternak <willi.pasternak@googlemail.com>
2012-05-21 15:55:54 +0200

    UnitTest fuer die Klasse Model.Geschaeftspartner hinzugefuegt.

--- MiKuVer2.Host/Program.cs
+++ MiKuVer2.Host/Program.cs
@@ -13,12 +13,12 @@ namespace MiKuVer2.Host
         /// </summary>
         static void Main()
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[] 
-			{ 
-				new Service1() 
-			};
-            ServiceBase.Run(ServicesToRun);
+            //ServiceBase[] ServicesToRun;
+            //ServicesToRun = new ServiceBase[] 
+            //{ 
+            //    new Service1() 
+            //};
+            //ServiceBase.Run(ServicesToRun);
         }
     }
 }
