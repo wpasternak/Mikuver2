a257def8d230eb7b28b51461e1563c92481d1d73
Willi Pasternak <willi.pasternak@googlemail.com>
2012-05-21 14:41:35 +0200

    Unittest fuer GeschaeftspartnerService erstellt.
    Benoetigte Frameworks und Dateien hinzugefuegt.

---
+++ MiKuVer2.Tests.GeschaeftspartnerTests/GeschaeftspartnerServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MiKuVer2.Tests.GeschaeftspartnerTests
+{
+    using MiKuVer2.Model;
+
+    using Moq;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    class GeschaeftspartnerServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            var mock = new Mock<IGeschaeftspertnerRepository>();
+        }
+    }
+}
