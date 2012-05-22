837a539951ccfc9202baebfc235818a36dca6318
Avedis Sagrjan <awesig@mail.ru>
2012-05-22 22:16:40 +0200

    Kategorieklasse hinzugefuegt

---
+++ MiKuVer2.Model/Kategorie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace MiKuVer2.Model
+{
+    [DataContract(IsReference = true)]
+    class Kategorie
+    {
+        /// <summary>
+        /// Die Id der Kategorie
+        /// </summary>
+        [DataMember]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Der Name der Kategorie
+        /// </summary>
+        [DataMember]
+        public string Name { get; set; }
+    }
+}
