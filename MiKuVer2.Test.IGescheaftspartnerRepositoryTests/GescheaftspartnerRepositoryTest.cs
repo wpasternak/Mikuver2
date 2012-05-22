e7506910ab6c8d9838883fb4e6baed45ed8187c2
Willi Pasternak <willi.pasternak@googlemail.com>
2012-05-22 11:50:31 +0200

    Setup und Teardown implementiert.

--- MiKuVer2.Test.IGescheaftspartnerRepositoryTests/GescheaftspartnerRepositoryTest.cs
+++ MiKuVer2.Test.IGescheaftspartnerRepositoryTests/GescheaftspartnerRepositoryTest.cs
@@ -5,7 +5,27 @@ using System.Text;
 
 namespace MiKuVer2.Test.IGescheaftspartnerRepositoryTests
 {
+    using MiKuVer2.Model;
+    using MiKuVer2.Repositories.Geschaeftspartner.Fake;
+
+    using NUnit.Framework;
+
+    [TestFixture]
     public class GescheaftspartnerRepositoryTest
     {
+        private IGeschaeftspartnerRepository sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            sut = new GescheaftspartnerRepositoryFake();
+        }
+
+        [TearDown]
+        public void ClearTest()
+        {
+            sut = null;
+        }
+
     }
 }
