321ff03500513ce13d859656ad47b29937ec0f67
Willi Pasternak <willi.pasternak@googlemail.com>
2012-05-31 13:39:48 +0200

    Start Methoden hinzugefuegt und Service so gestaltet das er auch ueber eine Konsole gestartet werden kann.

--- MiKuVer2.Host/Program.cs
+++ MiKuVer2.Host/Program.cs
@@ -11,14 +11,63 @@ namespace MiKuVer2.Host
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        static void Main()
+        static void Main(string[] args)
         {
-            //ServiceBase[] ServicesToRun;
-            //ServicesToRun = new ServiceBase[] 
-            //{ 
-            //    new Service1() 
-            //};
-            //ServiceBase.Run(ServicesToRun);
+             bool debug = args.Contains("debug");
+
+#if DEBUG
+            debug = true;
+#endif
+
+            if (debug)
+            {
+                try
+                {
+                    Console.WriteLine("WCF-Dienste starten...");
+                    var service = new WCFHost();
+                    service.Starten();
+                    Console.WriteLine();
+                    Console.WriteLine("WCF Services are running an waiting for request ...");
+
+                    //while (true)
+                    //{
+                    //    Console.WriteLine("To stop the Services just hit enter !! ...");
+
+                    //    ConsoleKey key = Console.ReadKey().Key;
+                    //    if (key == ConsoleKey.K)
+                    //    {
+                    //        if (service.Konfigurieren())
+                    //        {
+                    //            service.Stoppen();
+                    //            service.Starten();
+                    //        }
+                    //    }
+                    //    else
+                    //    {
+                    //        break;
+                    //    }
+                    //}
+
+                    Console.ReadKey();
+
+                    Console.WriteLine("Dienst wird beendet...");
+                    service.Stoppen();
+                    Console.WriteLine("WCF-Dienst bendet...");
+                }
+                catch (Exception ex)
+                {
+                    
+                    Console.WriteLine(ex);
+                    Console.ReadKey();
+                }
+                
+            }
+            else
+            {
+                var servicesToRun = new ServiceBase[] { new WCFHost() };
+
+                ServiceBase.Run(servicesToRun);
+            }
         }
     }
 }
