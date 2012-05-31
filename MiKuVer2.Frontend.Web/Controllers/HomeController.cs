86e0c1c90a531226a4aa8a47fb426c98493a4c15
Willi Pasternak <willi.pasternak@googlemail.com>
2012-05-31 14:12:09 +0200

    MVC3 Projekt fuer das Frontend hinzugefuegt.

---
+++ MiKuVer2.Frontend.Web/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiKuVer2.Frontend.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Willkommen zu MiKuVer2";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
