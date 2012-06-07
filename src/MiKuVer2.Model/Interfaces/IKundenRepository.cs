1903f20652617f8f952227b36b51ab0b5461599b
Avedis Sagrjan <awesig@mail.ru>
2012-06-07 15:24:26 +0200

    IKundenRepository hinzugefuegt

---
+++ src/MiKuVer2.Model/Interfaces/IKundenRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MiKuVer2.Model.Interfaces
+{
+    interface IKundenRepository
+    {
+        /// <summary>
+        /// Gibt alle eigenen Kunden zurueck
+        /// </summary>
+        /// <returns>Liste aller eigenen Kunden</returns>
+        List<Kunde> GetDirekteKunden();
+
+        /// <summary>
+        /// Gibt alle Kunden von allen Geschaeftspaertnern zurueck
+        /// </summary>
+        /// <returns>List aller Kunden</returns>
+        List<Kunde> GetAlleKunden();
+
+        /// <summary>
+        /// Gibt alle direkten Kunden eines anderen Geschaeftspartners
+        /// </summary>
+        /// <param name="id">Die Id des Kunden</param>
+        /// <returns>Liste direkter Kunden eines Geschaeftspartners</returns>
+        List<Kunde> GetDirekteKundenVonGeschaeftspartner(int id);
+
+        /// <summary>
+        /// Gibt alle Kunden, eines Geschaeftspartners und aller seiner Geschaeftspartner, zurueck
+        /// </summary>
+        /// <param name="id">Die Id des Kunden</param>
+        /// <returns>Liste aller direkten und indirekten Kunden eines Geschaeftspartners</returns>
+        List<Kunde> GetKundenVonGeschaeftspartner(int id);
+
+        /// <summary>
+        /// Gibt einen Kunden zurueck
+        /// </summary>
+        /// <param name="id">Die Id des Kunden</param>
+        /// <returns>Einen Kunden</returns>
+        Kunde GetKunde(int id);
+
+        /// <summary>
+        /// Gibt einen Kunden zurueck
+        /// </summary>
+        /// <param name="name">Der Name des Kunden</param>
+        /// <returns>Einen Kunden</returns>
+        Kunde GetKunde(string name);
+
+        /// <summary>
+        /// Gibt einen Wert zurueck ob der Kunde gespeichert wurde
+        /// </summary>
+        /// <param name="neuerKunde">Der zu speichernde Kunde</param>
+        /// <returns>true oder false</returns>
+        bool KundeSpeichern(Kunde neuerKunde);
+
+        /// <summary>
+        /// Gibt einen Wert zurueck ob der Kunde aktualisiert wurde
+        /// </summary>
+        /// <param name="kunden">der vorhandene und zu aktualisierende Kunde</param>
+        /// <returns>true oder false</returns>
+        bool KundenAktualisieren(Kunde kunden);
+    }
+}
