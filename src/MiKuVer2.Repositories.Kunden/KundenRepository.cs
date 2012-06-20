54f8ef7a17ad110239bd4629ffcd7301ffb07fb2
Willi Pasternak <willi.pasternak@googlemail.com>
2012-06-20 13:11:44 +0200

    Fehler in Abfrage behoben

--- src/MiKuVer2.Repositories.Kunden/KundenRepository.cs
+++ src/MiKuVer2.Repositories.Kunden/KundenRepository.cs
@@ -39,7 +39,7 @@ namespace MiKuVer2.Repositories.Kunden
             var result = new List<Kunde>();
 
             MySqlCommand command = new MySqlCommand(
-                "SELECT k.ID, k.KundeSeit, p.Vorname, p.Nachname, p.Geburtsdatum, p.Fax, p.Telefon, p.Strasse, p.Hausnummer, p.PLZ, p.Ort, `p.E-Mail` FROM Kunden k, Person p WHERE geschaeftspartnerId=@id AND k.PersonId=p.ID", this.connection);
+                "SELECT k.ID, k.KundeSeit, p.Vorname, p.Nachname, p.Geburtsdatum, p.Fax, p.Telefon, p.Strasse, p.Hausnummer, p.PLZ, p.Ort, p.`E-Mail` FROM Kunden k, Person p WHERE geschaeftspartnerId=@id AND k.PersonId=p.ID", this.connection);
             command.Parameters.AddWithValue("@id", 1);
 
             var reader = command.ExecuteReader();
@@ -116,7 +116,7 @@ namespace MiKuVer2.Repositories.Kunden
             var kunde = new Kunde();
 
             MySqlCommand command = new MySqlCommand(
-                "SELECT k.ID, k.KundeSeit, p.Vorname, p.Nachname, p.Geburtsdatum, p.Fax, p.Telefon, p.Strasse, p.Hausnummer, p.PLZ, p.Ort, p.E-Mail FROM Kunden k, Person p WHERE k.ID=@id AND k.PersonId=p.ID", this.connection);
+                "SELECT k.ID, k.KundeSeit, p.Vorname, p.Nachname, p.Geburtsdatum, p.Fax, p.Telefon, p.Strasse, p.Hausnummer, p.PLZ, p.Ort, p.`E-Mail` FROM Kunden k, Person p WHERE k.ID=@id AND k.PersonId=p.ID", this.connection);
             command.Parameters.AddWithValue("@id", id);
 
             var reader = command.ExecuteReader();
